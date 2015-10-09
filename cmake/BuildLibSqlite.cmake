eec74396001569ca8006dece116ad2e18aef7779
Vladislav Shpilevoy <vshpilevoi@mail.ru>
2015-10-09 13:23:41 +0300

    SQLite as submodule from https://github.com/mackyle/sqlite

--- cmake/BuildLibSqlite.cmake
+++ cmake/BuildLibSqlite.cmake
@@ -1,11 +1,11 @@
 #
 # A macro to build the sqlite submodule
 macro(sqlite_build)
-    set(sqlite_src ${PROJECT_SOURCE_DIR}/third_party/sqlite/sqlite3.c)
+    set(sqlite_src ${PROJECT_SOURCE_DIR}/third_party/sqlite/build/sqlite3.c)
 
     add_library(sqlite STATIC ${sqlite_src})
 
-    set(LIBSQLITE_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/third_party/sqlite)
+    set(LIBSQLITE_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/third_party/sqlite/build/tsrc)
     set(LIBSQLITE_LIBRARIES sqlite)
 
     message(STATUS "Use bundled sqlite library: ${LIBSQLITE_LIBRARIES}")
