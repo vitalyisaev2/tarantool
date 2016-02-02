8bb62471b8713f4f09eae7912d1937d678db7707
Vladislav Shpilevoy <v.shpilevoy@corp.mail.ru>
2016-02-02 17:02:22 +0300

    Tcl tests.
    Signed-off-by: Vladislav Shpilevoy <v.shpilevoy@corp.mail.ru>

--- cmake/BuildLibSqlite.cmake
+++ cmake/BuildLibSqlite.cmake
@@ -4,7 +4,7 @@ macro(trnsqlite_build)
     set(sqlite_src ${PROJECT_SOURCE_DIR}/third_party/sqlite/build/sqlite3.c)
 
     add_library(trnsqlite SHARED ${sqlite_src})
-    SET_TARGET_PROPERTIES(trnsqlite PROPERTIES COMPILE_FLAGS "-fPIC")
+    SET_TARGET_PROPERTIES(trnsqlite PROPERTIES COMPILE_FLAGS "-fPIC -DSQLITE_PRIVATE=\"\" -DSQLITE_TEST=1 -DSQLITE_CORE")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0 -g")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -g")
 	include_directories(${PROJECT_SOURCE_DIR}/third_party/sqlite/build/tsrc)
