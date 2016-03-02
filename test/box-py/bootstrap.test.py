1fd28ac1d22ed0012743c80930d50f48a7270a9e
Roman Tsisyk <roman@tsisyk.com>
2016-03-02 16:13:18 +0300

    Fix #1281: upgrade script 1.6.5 -> 1.6.8
    Use dofile('./extra/schema.lua').upgrade() to upgrade.
    
    ./extra/schema_fill.lua and ./extra/schema_erase.lua were
    merged to ./extra/schema.lua

--- test/box-py/bootstrap.test.py
+++ test/box-py/bootstrap.test.py
@@ -9,7 +9,8 @@ cluster_uuid = yaml.load(server.admin('box.space._schema:get("cluster")',
 sys.stdout.push_filter(cluster_uuid, '<cluster uuid>')
 sys.stdout.push_filter(server.sourcedir, '<sourcedir>')
 
-server.admin('dofile("%s/extra/schema_erase.lua")' % server.sourcedir)
+server.admin('schema = dofile("%s/extra/schema.lua")' % server.sourcedir)
+server.admin('schema.erase()')
 server.admin('box.space._schema:select{}')
 server.admin('box.space._cluster:select{}')
 server.admin('box.space._space:select{}')
@@ -18,7 +19,7 @@ server.admin('box.space._user:select{}')
 server.admin('box.space._func:select{}')
 server.admin('box.space._priv:select{}')
 
-server.admin('dofile("%s/extra/schema_fill.lua")' % server.sourcedir)
+server.admin('schema.upgrade()')
 server.admin("box.snapshot()")
 server.restart()
 
